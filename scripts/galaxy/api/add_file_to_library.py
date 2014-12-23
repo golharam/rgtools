ae0664cd09e7e407964b397796c5c390797c174d
Ryan Golhar <ryan.golhar@bms.com>
2014-12-23 18:22:55 -0500

    Added script to add files to Galaxy

--- scripts/galaxy/api/add_file_to_library.py
+++ scripts/galaxy/api/add_file_to_library.py
@@ -17,69 +17,88 @@ from common import display, submit
 import os
 import sys
 
-api_url = ''
-api_key = ''
-file = ''
-library = ''
+_api_url = ''
+_api_key = ''
+_file = ''
+_libraryPath = ''
 _debug = 0
    
-def getFileMetaData(file_id):
-    metadata = display(api_key, api_url + '/api/libraries/datasets/%s' % file_id, return_formatted = False)
-    return metadata
-    
-def isFileInGalaxyLibrary(library_id, file):
-    library_contents = display(api_key, api_url + "/api/libraries/%s/contents" % library_id, return_formatted = False)
-    for entry in library_contents:
+def isFileInGalaxyFolder(folder, file):
+    folder_contents = display(_api_key, _api_url + "/api/folders/%s/contents" % folder['id'], return_formatted = False)
+    for entry in folder_contents['folder_contents']:
         if entry['type'] == 'file':
-            libraryfile_metadata = getFileMetaData(entry['id'])
-            libraryfile_fullpath = libraryfile_metadata['file_name']     
-            if (file == libraryfile_fullpath):
+            file_metadata = display(_api_key, _api_url + '/api/libraries/datasets/%s' % entry['id'], return_formatted = False)
+            file_fullpath = file_metadata['file_name']     
+            if (file == file_fullpath):
                 return 1
     return 0
-    
+   
 """ 
 The libraryName can either be just a library, or a library and folder path.
 If the libraryName contains a folder path, we need to find the library, then
 find the folder within the library to get its path.
-"""    
-def getGalaxyLibrary(libraryName):
-    libs = display(api_key, api_url + '/api/libraries', return_formatted=False)
+
+Algorithm:
+1.  Split up the libraryFolderPath to get the library and folder path as two 
+    separate values
+2.  Make sure the library exists and get its contents
+3.  Find the folder in question and return it.  If it is the root folder, it
+    will be "/", else the folder path will be what we are looking for.
+""" 
+def getGalaxyFolderFromLibrary(library, folderPath):
+    library_contents = display(_api_key, _api_url + "/api/libraries/%s/contents" % library['id'], return_formatted = False)
+    for entry in library_contents:
+        if entry['name'] == folderPath:
+            return entry
+
+    print 'Unable to locate folder %s in library %s' % (folderPath, library['name'])
+    sys.exit(1)
+    
+def getGalaxyLibrary(libraryName):  
+    libs = display(_api_key, _api_url + '/api/libraries', return_formatted=False)
     for library in libs:
         if library['name'] == libraryName:
             return library
+        
     print 'Unable to locate library %s in Galaxy' % libraryName
-    return 0
+    sys.exit(1)
     
 def main():
-    print 'Galaxy API URL: %s' % api_url
-    print 'Galaxy API Key: %s' % api_key
-    print 'File: %s' % file
-    print 'Library: %s' % library
+    print 'Galaxy API URL: %s' % _api_url
+    print 'Galaxy API Key: %s' % _api_key
+    print 'File: %s' % _file
+    print 'Library: %s' % _libraryPath
     print ''
-    if not os.path.isfile(file):
+    if not os.path.isfile(_file):
         print 'Unable to location file'
         sys.exit(1)
+
+    fields = split(_libraryPath, '/')
+    libraryName = fields[0]
+    if len(fields) == 1:
+        folderPath = '/'
+    else:
+        sep = '/'
+        folderPath = '/' + sep.join(fields[1:])
     
-    library_data = getGalaxyLibrary(library) 
-    if library_data == 0:
-        print '%s is not a library in Galaxy' % library
-        sys.exit(1)
-        
-    if isFileInGalaxyLibrary(library_data['id'], file):
+    library = getGalaxyLibrary(libraryName)
+    folder = getGalaxyFolderFromLibrary(library, folderPath)
+         
+    if isFileInGalaxyFolder(folder, _file):
         print 'File already exists in Galaxy library'
         sys.exit(1)
     
-    print 'Adding %s to %s' % (file, library)
+    print 'Adding %s to %s' % (_file, library['name'])
     data = {}
-    data['folder_id'] = library_data['root_folder_id']
+    data['folder_id'] = folder['id']
     data['create_type'] = 'file'    
     data['file_type'] = 'auto'
     data['dbkey'] = ''
     data['upload_option'] = 'upload_paths'
-    data['filesystem_paths'] = file
+    data['filesystem_paths'] = _file
     data['link_data_only'] = 'link_to_files'
     
-    libset = submit(api_key, api_url + "/api/libraries/%s/contents" % library_data['id'], data, return_formatted = True)
+    libset = submit(_api_key, _api_url + "/api/libraries/%s/contents" % library['id'], data, return_formatted = True)
     print libset
 
     
@@ -90,9 +109,9 @@ if __name__ == '__main__':
     parser.add_argument('file', help="Full path of file to add")
     parser.add_argument('library', help="Galaxy library to add file to")
     args = parser.parse_args()
-    api_key = args.api_key
-    api_url = args.api_url
-    file = args.file
-    library = args.library
+    _api_key = args.api_key
+    _api_url = args.api_url
+    _file = args.file
+    _libraryPath = args.library
     main()
 
