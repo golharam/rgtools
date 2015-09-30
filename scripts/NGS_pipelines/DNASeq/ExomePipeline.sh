c9a3a89c2b9b87c6d6837464aba46a853724038e
Ryan Golhar <ryan.golhar@bms.com>
2015-09-30 13:29:39 -0400

    Translating BDS script back to BASH for AWS

--- scripts/NGS_pipelines/DNASeq/ExomePipeline.sh
+++ scripts/NGS_pipelines/DNASeq/ExomePipeline.sh
@@ -1,320 +1,300 @@
 #!/bin/bash
 
-# SGE Grid Engine Parameters
 #$ -S /bin/sh
-#$ -pe pe 4
+#$ -pe orte 8
 #$ -j y
 #$ -cwd
 
-# Example to run:
-# FASTQ files are assumed to be in $BASEDIR as specified in exome_common_parameters.ini
-# qsub -v PARAMETERS_FILE=`pwd`/exome_common_parameters.ini,SAMPLE_NAME=...,FASTQ_1=... `pwd`/ExomePipeline.sh
+##############################################################################
+# Set default values.  These can be set via command-line options only.  
+# Setting them here overrides environment variables
+###############################################################################
+VERSION="0.1"
+HELP=0
+THREADS=8
 
-VERSION="v0.1"
-
-echo "Running Exome Analysis Pipeline ($VERSION) on `uname -n`"
-date '+%m/%d/%y %H:%M:%S'
-echo
-
-##############################################################
-##  Exome Analysis Pipeline 		                    ##
-##############################################################
-
-# We can define this as an environment variable.
-if [ -z ${PARAMETERS_FILE} ]
+if [ $# -eq 0 ]
 then
-#	Because each project may have different parameters, its better load 
-#	a project specific file from the current directory, if its not
-#       specified.
-	PARAMETERS_FILE=./exome_common_parameters.ini
-fi
-
-if [ ! -e ${PARAMETERS_FILE} ] 
-then
-	echo "Unable to locate ${PARAMETERS_FILE}"
-	exit -1
-fi
-
-echo "Using parameters file $PARAMETERS_FILE"
-date '+%m/%d/%y %H:%M:%S' 
-echo
-. ${PARAMETERS_FILE}
-
-##############################################################
-##  Check for SAMPLE_NAME env var                           ##
-##############################################################
-
-if [ -z "$SAMPLE_NAME" ]
-then
-	echo "SAMPLE_NAME is not set.  Set as environment variable."
-	exit 1
-fi
-
-echo Analyzing Sample ${SAMPLE_NAME}
-date '+%m/%d/%y %H:%M:%S' 
-echo
-
-##############################################################
-##  Make analysis directory                                 ##
-##############################################################
-
-if [ -z "$BASE_DIR" ]
-then
-	echo "BASE_DIR is not set"
-	exit 1
-fi
-
-if [ ! -d "${BASE_DIR}" ] 
-then
-	echo "$BASE_DIR does not exist"
-	exit -1
-fi
-
-cd ${BASE_DIR}
-
-#if [ ! -d "${SAMPLE_NAME}" ] 
-#then
-#	mkdir ${SAMPLE_NAME}
-#fi
-
-#cd ${SAMPLE_NAME}
-
-echo Using Analysis Directory: `pwd` 
-date '+%m/%d/%y %H:%M:%S'
-echo
-
-##############################################################
-## Print out version of tools used in this analysis         ##
-##############################################################
-
-echo The following applications will be used:
-echo fastqValidator: ${FASTQ_VALIDATOR}
-echo
-echo FastQC: ${FASTQC}
-echo
-echo BWA: ${BWA}
- ${BWA} 2>&1 | head -n 3 | tail -n 1
-echo BWA Reference: ${BWA_REFERENCE}
-echo
-echo Java: ${JAVA}
- ${JAVA} -version 2>&1 | head -n 1
-echo
-echo PicardTools: ${PICARD_TOOLS}
-echo
-echo GATK: ${GATK_JAR}
-echo GATK Version `${JAVA} -jar $GATK_JAR -version`
-echo
-echo SAMTools: ${SAM_TOOLS}
-echo SAMTools `${SAM_TOOLS} 2>&1 | head -n 3 | tail -n 1`
-echo
-
-##############################################################
-##  Create/Read ini file (if it doesn't already exist)      ##
-##############################################################
-
-if [ -e ${SAMPLE_NAME}.ini ]
-then
-        echo "Reading previous sample settings: "
-	echo `pwd`/${SAMPLE_NAME}.ini
-        date '+%m/%d/%y %H:%M:%S'
-        echo ---
-        cat ${SAMPLE_NAME}.ini
-        echo ---
-	echo 
-
-        source ./${SAMPLE_NAME}.ini
-fi
-
-##############################################################
-##  Check for FASTQ files and read group information        ##
-##############################################################
-
-if [ -z "${FASTQ_1}" ]
-then
-	echo "FASTQ_1 not defined."
-	date '+%m/%d/%y %H:%M:%S'
-	exit -1
-fi
-
-if [ ! -e ${FASTQ_1} ] 
-then
-	echo Unable to locate FASTQ_1: $FASTQ_1 
-	echo in relation to analysis directory. Specify full path to FASTQ file.
-	date '+%m/%d/%y %H:%M:%S'
-	echo
-	exit -1	
-fi
-
-##############################################################
-## Count # of Reads (assuming fastq is compressed with gz)  ##
-##############################################################
-
-if [ -z "$COUNT_READ1" ]
-then
-	echo "Validating forward reads (${FASTQ_1})"
-	date '+%m/%d/%y %H:%M:%S'
-	
-	lines=`$FASTQ_VALIDATOR --file ${FASTQ_1}`
-	if [ $? -ne 0 ]
+	if [ -z "$SAMPLE" ]
 	then
-		echo $lines
-		echo "$FASTQ_1 is not valid"
+		HELP=1
+	fi
+fi
+
+###############################################################################
+# Get command line options.
+# a.  Use > 1 to consume two arguments per pass in the loop (e.g. each argument
+#     has a corresponding value to go with it).
+# b.  Use > 0 to consume one or more arguments per pass in the loop (e.g. some
+#     arguments don't have a corresponding value to go with it such as --help).
+###############################################################################
+while [[ $# > 0 ]]
+do
+	key="$1"
+	case $key in
+		-1|--fastq1)
+		FASTQ1="$2"
+		shift # past argument
+		;;
+
+		-2|--fastq2)
+		FASTQ2="$2"
+		shift # past argument
+		;;
+
+		-a|--aws)
+		AWS=1
+		;;
+
+		-h|--help)
+		HELP=1
+		;;
+
+		-s|--sample)
+		SAMPLE="$2"
+		shift # past argument
+		;;
+
+		--tmpdir)
+		TMP_DIR="$2"
+		shift # past argument
+		;;
+
+		*)
+		echo "Unknown option: $key"
+		exit -1
+		;;
+	esac
+	shift	# past argument or value
+done
+
+if [ $HELP == 1 ]; then
+	echo "Usage 1: $0 <options> <usage>"
+	echo "where usage:"
+	echo "Usage 1: <options> [-s|--sample <sample name>] [-1|--fastq1 <path/to/fastq1>] [-2|--fastq2 <path/to/fastq2>]"
+	echo "Options:"
+	echo "	-a|--aws"
+	echo "	-h|--help"
+	echo "  --tmpdir <scratch space>"
+	exit 0
+fi
+
+# If variables are not set as an environment variable or as a parameter, set them to default here.
+if [ -z "$AWS" ]; then
+	AWS=0
+fi
+if [ -z "$TMP_DIR" ]; then
+	TMP_DIR=/scratch
+fi
+
+if [ $AWS -eq 1 ]; then
+	EXT_PKGS_DIR=/ngs/apps
+	REFERENCE_DIR=/ngs/reference
+else
+	EXT_PKGS_DIR=/apps/sys/galaxy/external_packages
+	REFERENCE_DIR=/ng18/galaxy/reference_genomes	
+fi
+
+# Applications / Programs
+BWA=$EXT_PKGS_DIR/bwa-0.7.12/bwa
+FASTQC=$EXT_PKGS_DIR/FastQC-0.11.3-RG/fastqc
+FASTQVALIDATOR=$EXT_PKGS_DIR/fastQValidator-0.1.1a/bin/fastQValidator
+
+# Reference data
+SPECIES=hg19
+REFERENCE="$REFERENCE_DIR/$SPECIES/$SPECIES.fa"
+BWA_REFERENCE="$REFERENCE_DIR/$SPECIES/bwa_index/$SPECIES"
+
+# Start Analysis
+echo "Processing $SAMPLE"
+date '+%m/%d/%y %H:%M:%S'
+echo "Pipeline: $VERSION"
+echo "TmpDir: $TMP_DIR"
+echo "AWS: $AWS"
+echo
+
+# Make tmp working directory aka SAMPLE_DIR
+SAMPLE_DIR=/scratch/$SAMPLE
+#if [ -z "$SAMPLE_DIR" ]
+#then
+#	cd $TMP_DIR
+#	SAMPLE_DIR=`mktemp -d --tmpdir=${TMP_DIR} ${SAMPLE}_XXXXXX`
+	if [ ! -d $SAMPLE_DIR ]
+	then
+	        mkdir $SAMPLE_DIR
+	fi
+#fi
+cd $SAMPLE_DIR
+echo "Working in `uname -n`:`pwd`"
+date '+%m/%d/%y %H:%M:%S'
+echo
+
+##############################################################################
+# Step 1: Download the file to the local scratch space
+# If AWS, copy from S3, FASTQ1, FASTQ2 
+# Else assume file is local and unknown if compressed or not.  
+# Regardless, $FASTQ1 and $FASTQ2 point to the files.
+##############################################################################
+
+if [ $AWS -eq 1 ] 
+then
+	if [ ! -e ${SAMPLE}_1.fastq.gz ]
+	then
+		echo "Downloading from S3: $FASTQ1"
+		date '+%m/%d/%y %H:%M:%S'
+		echo
+
+		aws s3 cp $FASTQ1 ${SAMPLE}_1.fastq.gz
+	
+		if [ $? -ne 0 ]
+		then
+		        echo "Error downloading $FASTQ1"
+		        rm -f ${SAMPLE}_1.fastq.gz 2>/dev/null
+		        exit -1
+		fi
+	fi
+	FASTQ1=`pwd`/${SAMPLE}_1.fastq.gz
+	
+	if [ ! -e ${SAMPLE}_2.fastq.gz ]
+	then
+		echo "Downloading from S3: $FASTQ2"
+		date '+%m/%d/%y %H:%M:%S'
+		echo
+	
+		aws s3 cp $FASTQ2 ${SAMPLE}_2.fastq.gz
+	
+		if [ $? -ne 0 ]
+		then
+		        echo "Error downloading $FASTQ2"
+		        rm -f ${SAMPLE}_2.fastq.gz 2>/dev/null
+		        exit -1
+		fi
+	fi
+	FASTQ2=`pwd`/${SAMPLE}_2.fastq.gz
+fi
+
+# If we didn't do any of the above, then FASTQ1 and FASTQ2 are local files.
+# They are either links to the original files or are the original files.
+# In either case, make a link to them so we don't accidentially delete them.
+
+
+echo "Using Fastq file(s):"
+if [ ! -e $FASTQ1 ]
+then
+	echo "Unable to locate $FASTQ1.  Make sure full path is provided"
+	exit -1
+fi
+echo FASTQ1: $FASTQ1
+if [ -n "$FASTQ2" ]
+then
+	if [ ! -e $FASTQ2 ]
+	then
+		echo "Unable to locate $FASTQ2.  Make sure full path is provided"
 		exit -1
 	fi
-	
-	COUNT_READ1=`echo "$lines" | cut -d ' ' -f 8`
-	
-	echo FASTQ_1=${FASTQ_1} >> ${SAMPLE_NAME}.ini
-	echo COUNT_READ1=${COUNT_READ1} >> ${SAMPLE_NAME}.ini
-
-	echo The forward or read1 fastq contains ${COUNT_READ1} reads
-	date '+%m/%d/%y %H:%M:%S'
-	echo
+	echo FASTQ2: $FASTQ2
 fi
 
-if [ -z "$COUNT_READ2" ]
-then
-        echo "Validating reverse reads (${FASTQ_2})"
-        date '+%m/%d/%y %H:%M:%S'
+date '+%m/%d/%y %H:%M:%S'
+analysis_date_started=$(date +"%s")
+echo
 
-        lines=`$FASTQ_VALIDATOR --file ${FASTQ_2}`
+##############################################################################
+# Step 2: Run FastQ Validator (make sure the fastq [zip] file is valid)
+##############################################################################
+if [ ! -e $SAMPLE.fq1Validator.txt ]
+then
+	echo "Validating forward reads ($FASTQ1)"
+	date '+%m/%d/%y %H:%M:%S'
+	echo
+
+	$FASTQVALIDATOR --disableSeqIDCheck --file $FASTQ1 > $SAMPLE.fq1Validator.txt
+
+	if [ $? -ne 0 ]
+	then
+		echo "$FASTQ1 is not valid"
+		rm $SAMPLE.fq1Validator.txt
+		exit -1
+	fi
+fi
+
+if [ -n "$FASTQ2" ] && [ ! -e $SAMPLE.fq2Validator.txt ]
+then
+        echo "Validating reverse reads ($FASTQ2)"
+        date '+%m/%d/%y %H:%M:%S'
+        echo
+
+	$FASTQVALIDATOR --disableSeqIDCheck --file $FASTQ2 > $SAMPLE.fq2Validator.txt
+
         if [ $? -ne 0 ]
         then
-                echo $lines
-                echo "$FASTQ_2 is not valid"
+                echo "$FASTQ2 is not valid"
+		rm $SAMPLE.fq2Validator.txt
                 exit -1
         fi
 
-        COUNT_READ2=`echo "$lines" | cut -d ' ' -f 8`
-
-        echo FASTQ_2=${FASTQ_2} >> ${SAMPLE_NAME}.ini
-        echo COUNT_READ2=${COUNT_READ2} >> ${SAMPLE_NAME}.ini
-
-        echo The reverse or read2 fastq contains ${COUNT_READ2} reads
-        date '+%m/%d/%y %H:%M:%S'
-        echo
 fi
 
-##############################################################
-## Run FastQC                                               ##
-##############################################################
-
-FASTQC_OUTFILE=`basename $FASTQ_1 $EXT`
-FASTQC_OUTFILE="${FASTQC_OUTFILE}_fastqc.html"
-if [ ! -e $FASTQC_OUTFILE ]
+##############################################################################
+# Step 2: Run FastQC
+##############################################################################
+if [ ! -e "${SAMPLE}_1_fastqc.zip" ]
 then
 	echo "Running FastQC"
 	date '+%m/%d/%y %H:%M:%S'
-
-	# TODO: Add --outfile
-	$FASTQC -t ${THREADS} --outdir=. $FASTQ_1 $FASTQ_2
-
 	echo
-	echo "Finished running FastQC"
-	date '+%m/%d/%y %H:%M:%S'
-	echo
+
+	$FASTQC --quiet --outdir=. --extract -t 2 $FASTQ1 $FASTQ2
+
+	if [ $? -ne 0 ]
+	then
+		echo "Error running FastQC"
+		rm -rf *_fastqc *_fastqc.zip *_fastqc.html
+	        exit -1
+	fi
 fi
 
 ##############################################################
-## Map Reads						    ##
+## Step 3: Map the sample				    ##
 ##############################################################
 
-if [ ! -e ${SAMPLE_NAME}_1.sai ]
+if [ ! -e ${SAMPLE}.sam ]
 then
-	echo Aligning forward reads 
+	echo "Running BWA"
 	date '+%m/%d/%y %H:%M:%S'
 	echo
 
-	echo ${BWA} aln -t ${THREADS} -f ${SAMPLE_NAME}_1.sai ${BWA_REFERENCE} ${FASTQ_1}
-	${BWA} aln -t ${THREADS} -f ${SAMPLE_NAME}_1.sai ${BWA_REFERENCE} ${FASTQ_1}
+	$BWA mem -t 8 -M -R '@RG\tID:$SAMPLE\tSM:$SAMPLE\tLB:$SAMPLE\tPL:illumina' $BWA_REFERENCE $FASTQ1 $FASTQ2 > ${SAMPLE}.sam
 
 	if [ $? -ne 0 ]
 	then
-		echo "Encountered error aligning forward reads"
-		rm ${SAMPLE_NAME}_1.sai
-		exit -1
+		echo "Error running BWA"
+		rm $SAMPLE.sam
+	        exit -1
 	fi
-
-	echo Finished aligning forward reads
-	date '+%m/%d/%y %H:%M:%S'
-	echo
 fi
 
-if [ ! -e ${SAMPLE_NAME}_2.sai ]
+##############################################################
+## Step 4.  Sort the BAM file	
+##############################################################
+
+if [ ! -e ${SAMPLE}.bam ]
 then
-        echo Aligning reverse reads
-        date '+%m/%d/%y %H:%M:%S'
-        echo
-
-        echo ${BWA} aln -t ${THREADS} -f ${SAMPLE_NAME}_2.sai ${BWA_REFERENCE} ${FASTQ_2}
-        ${BWA} aln -t ${THREADS} -f ${SAMPLE_NAME}_2.sai ${BWA_REFERENCE} ${FASTQ_2}
-
-        if [ $? -ne 0 ]
-        then
-                echo "Encountered error aligning reverse reads"
-                rm ${SAMPLE_NAME}_2.sai
-                exit -1
-        fi
-
-        echo Finished aligning reverse reads
-        date '+%m/%d/%y %H:%M:%S'
-        echo
+	$JAVA -Xmx4G -Djava.io.tmpdir=. -jar $PICARD SortSam SO=coordinate INPUT=${SAMPLE}.sam OUTPUT=${SAMPLE}.bam VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true
 fi
 
-if [ ! -e ${SAMPLE_NAME}.sam ]
-then
-	echo Converting sai to sam
-	date '+%m/%d/%y %H:%M:%S'
-	echo
-
-	${BWA} sampe -f ${SAMPLE_NAME}.sam -r "@RG\tID:${SAMPLE_NAME}\tLB:${SAMPLE_NAME}\tSM:${SAMPLE_NAME}\tPL:illumina" ${BWA_REFERENCE} ${SAMPLE_NAME}_1.sai ${SAMPLE_NAME}_2.sai ${FASTQ_1} ${FASTQ_2}
-
-	if [ $? -ne 0 ]
-	then
-		echo "Encountered error converting sai to sam"
-		rm ${SAMPLE_NAME}.sam
-		exit -1
-	fi
-
-	echo Finished converting to sam
-	date '+%m/%d/%y %H:%M:%S'
-	echo
-fi
-
-if [ ! -e ${SAMPLE_NAME}.bam ]
-then
-	echo Converting sam to bam
-	date '+%m/%d/%y %H:%M:%S'
-	echo
-
-	${JAVA} -Xmx4G -Djava.io.tmpdir=$TMPDIR -jar ${PICARD_TOOLS}/SortSam.jar \
-		SO=coordinate \
-		INPUT=${SAMPLE_NAME}.sam \
-		OUTPUT=${SAMPLE_NAME}.bam \
-		VALIDATION_STRINGENCY=LENIENT \
-		CREATE_INDEX=true
-
-	if [ $? -ne 0 ]
-	then
-		echo "Encountered error converting sam to BAM"
-		rm ${SAMPLE_NAME}.ba? 2>/dev/null
-		exit -1
-	fi
-
-	echo Finished converting sam to bam
-	date '+%m/%d/%y %H:%M:%S'
-	echo
-fi
-
-if [ ! -e ${SAMPLE_NAME}.alignment_summary_metrics.txt ]
+##############################################################
+## Step 5.  Collect alignment metrics		
+##############################################################
+if [ ! -e ${SAMPLE}.alignment_summary_metrics.txt ]
 then
 	echo Collection alignment summary metrics
 	date '+%m/%d/%y %H:%M:%S'
         echo
 
-	$JAVA -jar $PICARD_TOOLS/CollectAlignmentSummaryMetrics.jar \
-		I=${SAMPLE_NAME}.bam \
-		O=${SAMPLE_NAME}.alignment_summary_metrics.txt \
+	$JAVA -Xmx4G -Djava.io.tmpdir=. -jar $PICARD CollectAlignmentSummaryMetrics \
+		I=${SAMPLE}.bam \
+		O=${SAMPLE}.alignment_summary_metrics.txt \
 		LEVEL=SAMPLE \
 		R=$REFERENCE
 
@@ -331,16 +311,19 @@ then
 
 fi
 
-if [ ! -e ${SAMPLE_NAME}.dedup.bam ]
+##############################################################
+## Step 5.  Mark Duplicates
+##############################################################
+if [ ! -e ${SAMPLE}.dedup.bam ]
 then
-	echo Removing duplicates
+	echo Marking duplicates
 	date '+%m/%d/%y %H:%M:%S'
 	echo
 
-	${JAVA} -Djava.io.tmpdir=$TMPDIR -jar ${PICARD_TOOLS}/MarkDuplicates.jar \
-		INPUT=${SAMPLE_NAME}.bam \
-		OUTPUT=${SAMPLE_NAME}.dedup.bam \
-		METRICS_FILE=${SAMPLE_NAME}.dedup_metrics.txt \
+	$JAVA -Djava.io.tmpdir=. -jar $PICARD MarkDuplicates \
+		INPUT=${SAMPLE}.bam \
+		OUTPUT=${SAMPLE}.dedup.bam \
+		METRICS_FILE=${SAMPLE}.dedup_metrics.txt \
 		VALIDATION_STRINGENCY=LENIENT \
 		CREATE_INDEX=true
 
@@ -356,7 +339,43 @@ then
 	echo
 fi
 
+##############################################################
+## Step 6.  Collect target region coverage
+##############################################################
+if [ ! -e $SAMPLE.targetRegionCoverage.txt ]
+then
+	$JAVA -Xmx4G -Djava.io.tmpdir=. -jar $RGTOOLS/CalculateTargetRegionCoverage.jar B=${SAMPLE}.dedup.bam G=$TARGETREGIONS_BED O=$SAMPLE.targetRegionCoverage.txt
+fi
+
+##############################################################
+# Step 7. Collect hsMetrics and per target metrics
+##############################################################
+if [ ! -e $SAMPLE.hsmetrics.txt ]
+then
+	$JAVA -Xmx4G -jar $PICARD CalculateHsMetrics BI=$BAITREGIONS_INTERVAL TI=$TARGETREGIONS_INTERVAL REFERENCE_SEQUENCE=$REFERENCE I=${SAMPLE}.dedup.bam O=$SAMPLE.hsmetrics.txt PER_TARGET_COVERAGE=$SAMPLE.per_target_metrics.txt
+fi
+
+##############################################################################
+# 13.  Transfer data to final resting place
+##############################################################################
+cd ..
+if [ $AWS -eq 0 ]
+then
+	echo "Moving ${SAMPLE_DIR} to ${OUTDIR}/${SAMPLE}"
+	mv ${SAMPLE_DIR} ${OUTDIR}/${SAMPLE}
+else
+	aws s3 cp --recursive $SAMPLE_DIR s3://bmsrd-ngs-projects/${PROJECT}/${SAMPLE}
+	
+	if [ $? -ne 0 ]
+	then
+		echo Error copying $SAMPLE_DIR
+		exit -1
+	fi
+
+	#rm -rf $SAMPLE_DIR 2>/dev/null
+fi
+
 echo
-echo Finished analyzing ${SAMPLE_NAME} 
+echo Finished analyzing ${SAMPLE} 
 date '+%m/%d/%y %H:%M:%S' 
 echo
